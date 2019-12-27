2929038a227a9b1cf5fb19e6e63dcc9c046aa6be
Raivis-Git <raivukam2@gmail.com>
2019-12-27 19:44:41 +0200

    Add files via upload
    From work update

---
+++ EasyCalory-home/src/main/java/com/example/calories/model/json/JSONCalories.java
@@ -0,0 +1,31 @@
+package com.example.calories.model.json;
+
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+import java.io.Serializable;
+@JsonPOJOBuilder
+public class JSONCalories implements Serializable {
+
+    public JSONCalories (Long client_Id) {
+        this.client_Id = client_Id;
+    }
+
+    private Double calories = 0.0;
+    private Long client_Id;
+
+    public Long getClient_Id() {
+        return client_Id;
+    }
+
+    public void setClient_Id(Long client_Id) {
+        this.client_Id = client_Id;
+    }
+
+    public Double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(Double calories) {
+        this.calories = calories;
+    }
+}
